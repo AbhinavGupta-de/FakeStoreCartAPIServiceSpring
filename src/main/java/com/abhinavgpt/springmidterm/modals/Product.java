20bc616af51f9bc29dd137ce2042434cc63df13a
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-03-12 15:50:26 +0530

    finished cart api service

--- src/main/java/com/abhinavgpt/springmidterm/modals/Product.java
+++ src/main/java/com/abhinavgpt/springmidterm/modals/Product.java
@@ -1,2 +1,27 @@
-package com.abhinavgpt.springmidterm.modals;public class Product {
+package com.abhinavgpt.springmidterm.modals;
+
+public class Product {
+    private int productId;
+    private int productQuantity;
+
+    public Product(int productId, int productQuantity) {
+        this.productId = productId;
+        this.productQuantity = productQuantity;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
 }
--- src/main/java/com/abhinavgpt/springmidterm/services/CartService.java
+++ src/main/java/com/abhinavgpt/springmidterm/services/CartService.java
@@ -1,2 +1,28 @@
-package com.abhinavgpt.springmidterm.services;public interface CartService {
+package com.abhinavgpt.springmidterm.services;
+
+import com.abhinavgpt.springmidterm.modals.Cart;
+
+import java.util.List;
+
+public interface CartService {
+
+    List<Cart> getAllProducts();
+
+    Cart getCart(long id);
+
+    List<Cart> limitedCarts(long limit);
+
+    List<Cart> sortedCarts(String order);
+
+    List<Cart> inDateRange(String start, String end);
+
+    List<Cart> userCart(long userId);
+
+    void addNewCart(Cart cart);
+
+    void updateProduct(Cart cart);
+
+    void deleteCart(long id);
+
+
 }
